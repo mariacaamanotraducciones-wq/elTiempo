b58f021777ea48d9eabfbe2ece1c46590e558f6b
mariacaamanotraducciones-wq <maria.caamano.traducciones@gmail.com>
2025-11-29 11:11:01 +0100

    added search functionality using API and updated html to keep emoji

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "03e34f234b90fab95bd92od60988t445";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiURL).then(refreshWeather);
+}
+
 function handleSearchInput(event) {
   event.preventDefault();
   let searchResult = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchResult.value;
+  searchCity(searchResult.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchInput);
+
+searchCity("Paris");
